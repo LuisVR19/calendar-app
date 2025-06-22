59431b78999ab169cdae90bd1e5d69149c29de25
Luis <luiyi234552@gmail.com>
2025-06-22 01:38:37 -0600

    Correcion inputs

--- app/pages/auth/layout.tsx
+++ app/pages/auth/layout.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div style={{ padding: "2rem", minHeight: "100vh" }}>
-      <h1 style={{ textAlign: "center" }}>Área de Autenticación</h1>
       <main>{children}</main>
     </div>
   );
